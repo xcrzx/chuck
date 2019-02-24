8016086805159557c34767b38e71665570bb0478
Dmitry Shevchenko <dmshevch@gmail.com>
2019-02-24 17:27:48 +0100

    Add timer for adding a random joke to favorites

--- src/components/FavoriteJokes.tsx
+++ src/components/FavoriteJokes.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import * as R from 'ramda'
 import { connect, ResolveThunks } from 'react-redux'
+import Button from '@material-ui/core/Button'
 
 import { Joke, StoreState } from '../types'
-import { fetchJokes } from '../store/allJokes'
+import { fetchJokes, fetchRandomJokes } from '../store/allJokes'
+import { addToFavorites } from '../store/favoriteJokes'
 
 import JokesLayout from './JokesLayout'
 
@@ -18,13 +20,22 @@ type FavoriteJokesProps = FavoriteJokesConnectProps & FavoriteJokesDispatchProps
 
 type FavoriteJokesState = {
   error?: Error
+  isFetching: boolean
 }
 
 class FavoriteJokes extends React.Component<FavoriteJokesProps, FavoriteJokesState> {
-  state: FavoriteJokesState = {}
+  state: FavoriteJokesState = {
+    isFetching: false,
+  }
+
+  timer: number | undefined
 
   componentDidMount() {
     this.fetchJokes()
+  }
+
+  componentWillUnmount() {
+    this.stopTimer()
   }
 
   fetchJokes = async () => {
@@ -38,11 +49,52 @@ class FavoriteJokes extends React.Component<FavoriteJokesProps, FavoriteJokesSta
     }
   }
 
+  toggleTimer = () => {
+    if (this.timer) {
+      this.stopTimer()
+    } else {
+      this.startTimer()
+    }
+  }
+
+  startTimer = () => {
+    this.setState({ isFetching: true })
+    this.timer = setTimeout(this.fetchRandomJokes, 5000)
+  }
+
+  stopTimer = () => {
+    clearTimeout(this.timer)
+    this.timer = undefined
+    this.setState({ isFetching: false })
+  }
+
+  fetchRandomJokes = async () => {
+    const jokes = await this.props.fetchRandomJokes(1)
+    this.props.addToFavorites(jokes[0].id)
+
+    if (this.props.favorites.length < 10) {
+      this.startTimer()
+    } else {
+      this.stopTimer()
+    }
+  }
+
   render() {
     const { jokes } = this.props
-    const { error } = this.state
+    const { error, isFetching } = this.state
 
-    return <JokesLayout error={error} title={'Favorite Jokes'} jokes={jokes} />
+    return (
+      <JokesLayout
+        error={error}
+        title={'Favorite Jokes'}
+        jokes={jokes}
+        actions={
+          <Button onClick={this.toggleTimer} size="small" color="primary">
+            {isFetching ? 'Stop fetching' : 'Fetch random jokes'}
+          </Button>
+        }
+      />
+    )
   }
 }
 
@@ -55,7 +107,7 @@ const mapStateToProps = (state: StoreState) => {
   }
 }
 
-const mapDispatchToProps = { fetchJokes }
+const mapDispatchToProps = { fetchJokes, fetchRandomJokes, addToFavorites }
 
 export default connect(
   mapStateToProps,
